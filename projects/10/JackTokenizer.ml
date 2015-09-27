8fcf7187dcc2e6d28b301b409d392c8c4ea306da
Dan Genin <dan.genin@gmail.com>
2015-09-27 14:55:01 -0400

    More pretty printing for subroutines

--- projects/10/JackTokenizer.ml
+++ projects/10/JackTokenizer.ml
@@ -22,7 +22,7 @@ let forward cl = cl.current <- cl.current + 1 ;;
 let forward_n cl n = cl.current <- cl.current + n ;;
 let extract pred cl =
   let st = cl.string and pos = cl.current in
-  let rec ext n = if n < cl.size && (pred (String.sub st n (cl.size - n))) then ext (n + 1) else n in
+  let rec ext n = if n < cl.si+ze && (pred (String.sub st n (cl.size - n))) then ext (n + 1) else n in
   let res = ext pos
   in cl.current <- res ; String.sub cl.string pos (res - pos) ;;
 
@@ -173,13 +173,15 @@ let rec declaration_print decl =
      List.map declaration_print class_subs;
      print_string ""
   | Dclass_var (var_scope, var_type, var_name) -> print_string (var_name ^ " " ^ (type_to_string var_type) ^ " " ^(scope_to_string var_scope) ^ "\n")
-  | Dsub_param (var_type, var_name) -> print_string ((type_to_string var_type) ^ " " ^ var_name)
+  | Dsub_param (var_type, var_name) -> print_string ((type_to_string var_type) ^ " " ^ var_name ^ " ")
   | Dsub (sub_type, ret_type, sub_name, param_list, var_list, body) -> 
      print_string ("Subroutine: " ^ sub_name ^ "\n");
-     print_string ((sub_type_to_string sub_type) ^ " " ^ (type_to_string ret_type));
-								       let _ = List.map declaration_print param_list in
-								       let _ = List.map declaration_print var_list in
-								       print_string "end sub declaration"
+     print_string ("Scope: " ^ (sub_type_to_string sub_type) ^ "\n");
+     print_string ("Returns: " ^ (type_to_string ret_type) ^ "\n");
+     print_string ("Params: ")
+     let _ = List.map declaration_print param_list in
+     let _ = List.map declaration_print var_list in
+     print_string "end sub declaration"
   | _ -> print_string "whatever";;
 
 exception ParserError of string;;
