2c266e86c976982bf28db99a1c1c26116913f195
ilya <ilya@seoul>
2016-01-10 17:28:29 -0500

    Expanded state machine to handle static declarations, updated design doc.
    The system doesn't currently work for the sample test program.

--- projects/10/JackTokenizer.ml
+++ projects/10/JackTokenizer.ml
@@ -424,5 +424,13 @@ let rec parse prog =
 let prog = (read_prog "") in
 let l = init_lex prog in
 (* in declaration_print (parse l);; *)
-let state_machine = [[(Lsymbol("{"), 1)]; [(Lsymbol("}"),2); (Lany,1)]] in
+(* let state_machine = [[(Lsymbol("{"), 1)]; [(Lsymbol("}"),2); (Lany,1)]] in *)
+let state_machine = [[(Lsymbol("{"), 1)]; 
+                     [(Lkeyword("static"), 2);(Lsymbol("}"),5); (Lany,1)]; 
+                     [(Lkeyword("char"),3); (Lkeyword("int"), 3); (Lkeyword("boolean"), 3); (Lkeyword("void"), 3)];
+                     (* [(Lident, 4)]; *)
+                     [(Lany, 4)];
+                     [(Lsymbol(","),3); (Lsymbol(";"),1)];
+                     [(Lsymbol("}"),2)];
+                    ]; in
 scanner state_machine l
