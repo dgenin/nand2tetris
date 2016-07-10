d41264482d54ce11399fb11cc6b5873e1d3d8893
Ilya Chukhman <ilya.chukhman@gmail.com>
2016-07-10 10:00:40 -0400

    Made main program take jack file as cmdline argument (instead of stdin)

--- projects/10/src/jc.ml
+++ projects/10/src/jc.ml
@@ -1,17 +1,32 @@
 
 (* Read in the program text from stdin *)
-let rec read_prog s =
+let read_prog ch =
+  let s = ref "" in
   try
-    let l = (s ^ (input_line stdin)) in read_prog l
+    while true do
+      s := (!s ^ (input_line ch))
+    done;
+    ""
   with
-    End_of_file -> s;;
+    End_of_file -> !s;;
 
-let prog = (read_prog "") in
+let prog =
+  match Sys.argv with
+    [|_;infile|] ->
+    begin
+      try
+        let ch = open_in infile in
+        let prog = (read_prog ch) in
+        close_in ch;
+        prog
+      with
+        e -> print_string "error occured"; print_endline (Printexc.to_string e); exit 1
+    end
+  | _ -> raise (Invalid_argument ("Usage: " ^ Sys.argv.(0) ^ " infile"))
+in
+
 let l = new Lexer.lexeme_list prog in
-(* List.map Lexer.lexeme_print l#get_list; *)
-(* in declaration_print (parse l);; *)
-(* let state_machine = [[(Lsymbol("{"), 1)]; [(Lsymbol("}"),2); (Lany,1)]] in *)
 
 (* Parse program and print the resulting token list *)
 let dclasses = Parser.scanner l in
-  List.iter Parser.declaration_print dclasses;;
+  List.iter (fun c -> Parser.declaration_print c; print_newline ()) dclasses;;
