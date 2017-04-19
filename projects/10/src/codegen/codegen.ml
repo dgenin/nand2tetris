6d415e65d734ad435b58b07eb3a4c6bcf2e6367b
ilya <ilya@seoul>
2017-04-19 18:44:20 -0400

    Added symbol table per Dan's work

--- projects/10/src/codegen/codegen.ml
+++ projects/10/src/codegen/codegen.ml
@@ -1,23 +1,50 @@
 type sym = 
     Svar
   | Sfield
-  | Ssub
-  | Smethod
+  | Sarg  
+  | Sstat
   ;;
 
-class scope ast  par =
-    let symt = BatHashtbl.create 100 in
+open BatHashtbl
+
+class symbol_table () =
+    let ct = create 50 in
+    let st = create 0 in
     object (s)
-        val symtab = symt
-        val mutable children = []
-        val parent = par (* need to use sub-ast*)
+        val classt = ct
+        val mutable subt = st
+
+        method start_subroutine = 
+            subt <- create 50;
+        method define (name:string) (typ:string) (kind:sym) = 
+            match kind with
+            | Sfield | Sstat -> add classt name (typ, kind, (length classt) + 1)
+            | Sarg | Svar -> add subt name (typ, kind, (length subt) + 1)
+        method kind_of name =
+            match (find_option subt name) with
+            | Some (_, kind, _) -> Some(kind)
+            | None -> match (find_option classt name) with
+                | Some(_, kind, _) -> Some(kind)
+                | None -> None
+        method type_of name =
+            match (find_option subt name) with
+            | Some (typ, _, _) -> Some(typ)
+            | None -> match (find_option classt name) with
+                | Some(typ, _, _) -> Some(typ)
+                | None -> None
+        method index_of name =
+            match (find_option subt name) with
+            | Some (_, _, i) -> Some(i)
+            | None -> match (find_option classt name) with
+                | Some(_, _, i) -> Some(i)
+                | None -> None    
         (* TODO: Replace string/int with valid types*)
-        method has_sym (sym: string) : bool =
-            try
-                let _ = BatHashtbl.find symtab sym in true
-            with Not_found -> false
-        method get_sym (sym: string) : sym option =
-            try
-                BatHashtbl.find symtab sym
-            with Not_found -> None
 end;;
+
+let test () =
+    let symtab = new symbol_table () in
+    symtab#define "name" "type" Svar;
+    symtab#start_subroutine;
+    match (symtab#index_of "name") with
+        | Some(i) -> print_int i
+        | None -> print_endline "none?"
