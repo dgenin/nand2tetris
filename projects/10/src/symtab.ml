8e9d1775b2bc31473f0f4785eb2968e064c4c6ca
ilya <ilya@seoul>
2017-05-29 15:15:18 -0400

    Shortened use of long modules names.

--- projects/10/src/symtab.ml
+++ projects/10/src/symtab.ml
@@ -1,8 +1,7 @@
 open Batteries;;
 
-(*let string_of_declaration = Grammar_j.string_of_declaration;;*)
-
-exception ParserError of string;;
+module G = Grammar
+module BH = BatHashtbl
 
 type sym =
     Svar
@@ -11,54 +10,48 @@ type sym =
   | Sstat
   ;;
 
-let scope2sym s = 
-    match s with
-    | `FIELD -> Sfield
-    | `STATIC -> Sstat
-    | _ -> raise (Failure "bad scope")
-
 class symbol_table () =
     object (s)
-        val classt = BatHashtbl.create 50
-        val mutable subt = BatHashtbl.create 0
+        val classt = BH.create 50
+        val mutable subt = BH.create 0
 
         method start_subroutine () =
-            subt <- BatHashtbl.create 50;
+            subt <- BH.create 50;
 
-        method define (name:Grammar.identifier) (typ:Grammar.var_type) (kind:Grammar.var_scope) =
+        method define (name:G.identifier) (typ:G.var_type) (kind:G.var_scope) =
             match kind with
-            | `FIELD | `STATIC -> BatHashtbl.add classt name (typ, kind, (BatHashtbl.length classt) + 1)
-            | `ARG | `VAR -> BatHashtbl.add subt name (typ, kind, (BatHashtbl.length subt) + 1)
-        method var_count (kind:Grammar.var_scope) : int =
+            | `FIELD | `STATIC -> BH.add classt name (typ, kind, (BH.length classt) + 1)
+            | `ARG | `VAR -> BH.add subt name (typ, kind, (BH.length subt) + 1)
+        method var_count (kind:G.var_scope) : int =
             let ht_enum =
                 if kind = `FIELD || kind = `STATIC then
-                    BatHashtbl.enum classt
+                    BH.enum classt
                 else
-                    BatHashtbl.enum subt
+                    BH.enum subt
             in
-            let count_types (c, kind) ((_: Grammar.identifier), (v: (Grammar.var_type * Grammar.var_scope * int))) =
+            let count_types (c, kind) ((_: G.identifier), (v: (G.var_type * G.var_scope * int))) =
                 match v with
                     | (_, tkind, _) when kind = tkind -> (c+1, kind)
                     | _ -> (c, kind)
             in
-            let (count, _) = (*BatEnum.fold*) List.fold_left count_types (0, kind) [(`INT, ("foo", `STATIC, 5)) ](*ht_enum*) in
-            count
+            (*let (count, _) = List.fold_left count_types (0, kind) [(`INT, ("foo", `STATIC, 5)) ] in*)
+            let (count, _) = BatEnum.fold count_types (0, kind) ht_enum in count
         method kind_of name =
-            match (BatHashtbl.find_option subt name) with
+            match (BH.find_option subt name) with
             | Some (_, kind, _) -> Some(kind)
-            | None -> match (BatHashtbl.find_option classt name) with
+            | None -> match (BH.find_option classt name) with
                 | Some(_, kind, _) -> Some(kind)
                 | None -> None
         method type_of name =
-            match (BatHashtbl.find_option subt name) with
+            match (BH.find_option subt name) with
             | Some (typ, _, _) -> Some(typ)
-            | None -> match (BatHashtbl.find_option classt name) with
+            | None -> match (BH.find_option classt name) with
                 | Some(typ, _, _) -> Some(typ)
                 | None -> None
         method index_of name =
-            match (BatHashtbl.find_option subt name) with
+            match (BH.find_option subt name) with
             | Some (_, _, i) -> Some(i)
-            | None -> match (BatHashtbl.find_option classt name) with
+            | None -> match (BH.find_option classt name) with
                 | Some(_, _, i) -> Some(i)
                 | None -> None
 end;;
